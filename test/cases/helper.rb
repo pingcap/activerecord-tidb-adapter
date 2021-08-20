aaa2437a8d283eac81513b71844eba6b577d6940
Hooopo Wang <hoooopo@gmail.com>
2021-08-20 18:22:24 +0800

    fix missing supports_text_column_with_default

--- test/cases/helper.rb
+++ test/cases/helper.rb
@@ -66,6 +66,7 @@ end
   supports_insert_conflict_target?
   supports_optimizer_hints?
   supports_datetime_with_precision?
+  supports_text_column_with_default?
 ].each do |method_name|
   define_method method_name do
     ActiveRecord::Base.connection.public_send(method_name)
