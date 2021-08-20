2ef5d8efaa1d753cd8cd60cc355e28a3e5e046eb
Hooopo Wang <hoooopo@gmail.com>
2021-08-20 20:02:23 +0800

    fix legacy connection insert

--- lib/active_record/connection_adapters/tidb_adapter.rb
+++ lib/active_record/connection_adapters/tidb_adapter.rb
@@ -104,7 +104,7 @@ module ActiveRecord
         sql, binds = to_sql_and_binds(arel, binds)
         value = exec_insert(sql, name, binds, pk, sequence_name)
         return id_value if id_value.present?
-
+        return last_inserted_id(value) if arel.is_a?(String)
         model = arel.ast.relation.instance_variable_get(:@klass)
         pk_def = schema_cache.columns_hash(model.table_name)[pk]
         if pk_def&.default_function && pk_def.default_function =~ /nextval/
