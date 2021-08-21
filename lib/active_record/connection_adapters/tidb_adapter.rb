7516e608ca5ab98fa8d63d47d6148de8b84be883
Hooopo Wang <hoooopo@gmail.com>
2021-08-22 01:55:16 +0800

    extract database statements

--- lib/active_record/connection_adapters/tidb_adapter.rb
+++ lib/active_record/connection_adapters/tidb_adapter.rb
@@ -6,30 +6,9 @@ require 'active_record/connection_adapters/tidb/setup'
 require_relative '../../version'
 require_relative '../sequence'
 require_relative 'tidb/schema_statements'
+require_relative 'tidb/database_statements'
 
 ActiveRecord::ConnectionAdapters::Tidb.initial_setup
-
-module ActiveRecord
-  module ConnectionAdapters # :nodoc:
-    module DatabaseStatements
-      def insert(arel, name = nil, pk = nil, id_value = nil, sequence_name = nil, binds = [])
-        sql, binds = to_sql_and_binds(arel, binds)
-        value = exec_insert(sql, name, binds, pk, sequence_name)
-        return id_value if id_value.present?
-        return last_inserted_id(value) if arel.is_a?(String)
-        model = arel.ast.relation.instance_variable_get(:@klass)
-        pk_def = schema_cache.columns_hash(model.table_name)[pk]
-        if pk_def&.default_function && pk_def.default_function =~ /nextval/
-          query_value("SELECT #{pk_def.default_function.sub('nextval', 'lastval')}")
-        else
-          last_inserted_id(value)
-        end
-      end
-      alias create insert
-    end
-  end
-end
-
 
 module ActiveRecord
   module ConnectionHandling # :nodoc:
