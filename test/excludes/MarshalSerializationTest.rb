b8937fcb53747c08511740a450505f2f4a44b6c4
Hooopo Wang <hoooopo@gmail.com>
2021-08-23 14:29:40 +0800

    skip

--- test/excludes/MarshalSerializationTest.rb
+++ test/excludes/MarshalSerializationTest.rb
@@ -2,3 +2,5 @@
 
 exclude :test_deserializing_rails_6_0_marshal_basic, 'skip'
 exclude :test_deserializing_rails_6_0_marshal_with_loaded_association_cache, 'skip'
+exclude :test_deserializing_rails_6_1_marshal_with_loaded_association_cache, 'skip'
+exclude :test_deserializing_rails_6_1_marshal_basic, 'skip'
