02bc2fdc0139e56126e17d215304030e1ff95787
Hooopo Wang <hoooopo@gmail.com>
2021-08-23 18:26:17 +0800

    Fix exclude test

---
+++ test/excludes/ActiveRecord/Encryption/UniquenessValidationsTest.rb
@@ -0,0 +1,1 @@
+exclude :test_uniqueness_validations_work_when_using_old_encryption_schemes, 'skip'
--- test/excludes/LogSubscriberTest.rb
+++ test/excludes/LogSubscriberTest.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 exclude :test_vebose_query_logs, 'skip'
+exclude :test_verbose_query_logs, 'skip'
